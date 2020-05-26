b2cd39ae7392111e4c0f87a24127b79192350ffd
James Perez <danjame@hotmail.com>
2020-05-25 23:45:00 -0500

    RBA - multisurf: empezo implementacion de MultiSURF

--- SBR/RBA/multisurf.py
+++ SBR/RBA/multisurf.py
@@ -42,6 +42,129 @@ def MultiSURF(x, y, datos):
 
     #Precomputa las distancias entre todos los pares de instancias unicas dentro del conjunto de datos
     print("Pre-computando la matriz de distancias...")
+
+    matrizDistancias = calcularMatrizDistancias(x, datos)
+
+    print("Computado")
+
+    limitesMismaClase = datos.DEFenotipo
+
+    D = []
+    distanciasPromedio = []
+
+    for i in range(datos.numInstanciasEntrenamiento):
+        vectorDistancias = []
+        vectorDistancias = obtenerDistanciasIndividuales(i, datos, matrizDistancias)
+        distanciasPromedio.append(obtenerPromedio(vectorDistancias))
+        desviacionEstandar = obtenerDesviacionEstandar(vectorDistancias, distanciasPromedio[i])
+        D.append(desviacionEstandar/2.0)
+
+    # Haciendo un bucle en los atributos
+    for k in range(datos.numAtributos):
+        if datos.infoAtributos[k][0]: # Atributo continuo
+            minA = datos.infoAtributos[k][1][0]
+            maxA = datos.infoAtributos[k][1][1]
+
+        conteoCercaDe = 0
+        conteoFallidoCercaDe = 0
+        conteoLejosDe = 0
+        conteoFallidoLejosDe = 0
+
+        diferenciaCercaDe = 0 # Inicializando el puntaje en 0
+        diferenciaFallidosCercaDe = 0
+        diferenciaLejosDe = 0
+        diferenciaFallidosLejosDe = 0
+
+        for i in range(datos.numInstanciasEntrenamiento):
+            for j in range(i, datos.numInstanciasEntrenamiento):
+                if i != j and x[i][k] != datos.etiquetaDatosFaltantes and x[j][k] != datos.etiquetaDatosFaltantes:
+                    localizador = [i, j]
+                    localizador = sorted(localizador, reversed = True) # Accede a la mitad correcta de la tabla (resultado de redundancia de la tabla eliminada)
+                    d = matrizDistancias[localizador[0]][localizador[1]]
+
+                    # Cercanos
+                    if (d < distanciasPromedio[i] - D[i]):
+                        # Fenotipo discreto
+                        if (datos.fenotipoDiscreto):
+
+                            # El mismo endpoint
+                            if y[i] == y[j]:
+                                conteoCercaDe += 1
+
+                                if x[i][k] != x [j][k]:
+                                    # Atributo continuo (puntajes cercanos de atributo para fenotipos cercanos deben arrojar una penalidad mayor de atributos)
+                                    if datos.infoAtributos[k][0]:
+                                        diferenciaCercaDe -= (abs(x[i][k] - x[j][k]) / (maxA - minA))
+                                    
+                                    # Discreto
+                                    else:
+                                        diferenciaCercaDe -= 1
+                            
+                            # Diferente endpoint
+                            else:
+                                conteoFallidoCercaDe += 1
+
+                                if x[i][k] != x[j][k]:
+                                    # Atributo continuo (puntajes mas lejanos de atributos para fenotipos diferentes cercanos deben arrojar bonus de atributos mayores)
+                                    if datos.infoAtributos[k][0]:
+                                        diferenciaFallidosCercaDe += abs(x[i][k] - x[j][k]) / (maxA - minA)
+
+                                    # Discreto
+                                    else:
+                                        diferenciaFallidosCercaDe += 1
+
+                        # Fenotipo continuo
+                        else:
+                            if abs(y[i] - y[j]) < limitesMismaClase:
+                                conteoCercaDe += 1
+
+                                if x[i][k] != x[j][k]:
+                                    # Atributo continuo
+                                    if datos.infoAtributos[k][0]:
+                                        diferenciaCercaDe -= (abs(x[i][k] - x[j][k]) / (maxA - minA))
+                                    
+                                    # Discreto
+                                    else:
+                                        diferenciaCercaDe -= 1
+
+                            else:
+                                conteoFallidoCercaDe += 1
+
+                                if x[i][k] != x[j][k]:
+                                    # Atributo continuo
+                                    if datos.infoAtributos[k][0]:
+                                        diferenciaFallidosCercaDe += abs(x[i][k] - x[j][k]) / (maxA - minA)
+                                    
+                                    # Discreto
+                                    else:
+                                        diferenciaFallidosCercaDe += 1
+
+                    # Lejanos
+                    if (d > distanciasPromedio[i] + D[i]):
+                        # Endpoint discreto
+                        if datos.fenotipoDiscreto:
+                            pass
+
+                        # Endpoint continuo
+                        else:
+                            pass
+
+        proporcionCercanos = conteoCercaDe / float(conteoCercaDe + conteoFallidoCercaDe)
+        proporcionFallidos = conteoFallidoCercaDe / float(conteoCercaDe + conteoFallidoCercaDe)
+
+        # Aplicando un esquema de pesaje para balancear puntajes
+        diferencia = diferenciaCercaDe * proporcionFallidos + diferenciaFallidosCercaDe * proporcionCercanos
+
+        proporcionCercanos = conteoLejosDe / float(conteoLejosDe + conteoFallidoLejosDe)
+        proporcionFallidos = conteoFallidoLejosDe / float(conteoLejosDe + conteoFallidoLejosDe)
+
+        # Aplicando un esquema de pesaje para balancear puntajes
+        diferencia += diferenciaLejosDe * proporcionFallidos + diferenciaFallidosCercaDe + proporcionCercanos
+
+        listaPuntajes[k] += diferencia
+
+    return listaPuntajes
+
     
 def multiClaseMultiSURF(x, y, datos):
     pass
