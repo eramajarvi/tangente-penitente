37648cd2c3c0ac7bc02fcf7be648bd2beefce3d0
James Perez <danjame@hotmail.com>
2020-05-27 14:27:12 -0500

    labibliotecaria exporta perfiles de entr y prueba de forma aleatoria

--- helperLaBibliotecaria.m
+++ helperLaBibliotecaria.m
@@ -11,7 +11,7 @@ fprintf('La Bibliotecaria está organizando los datos de los perfiles óptimos..
 
 cantidadDias = 516;
 
-datasetTodosDias = zeros(cantidadDias * 720, 7);
+datasetTodosDias = zeros(cantidadDias * 720, 4);
 
 % Rutas para importar los perfiles optimos de forma recursiva con los datos
 % de todos los dias y guardar los datasets generados
@@ -38,9 +38,9 @@ for i = 1 : 1 : cantidadDias
     horasActivacion = tiempo./3600 + 6; % El rango es de 6:00h a 18:00h, se encuentra la hora exacta de activacion
     
     % Valores de las variables de interes cuando hay activacion de la irrigacion
-    entradasDia = [horasActivacion(1:length(Pben)); irradiancia(1:length(Pben));...
-        temperaturaAmbiente(1:length(Pben)); velocidadViento(1:length(Pben));...
-        temperaturaPanel(1:length(Pben)); Pben];
+    entradasDia = [irradiancia(1:length(Pben));...
+        temperaturaAmbiente(1:length(Pben)); ...
+        velocidadViento(1:length(Pben))];
     
     % dayTargets guarda el control de la irrigacion con dos valores:
     % 0 es cuando el sistema NO esta irrigado
@@ -50,12 +50,9 @@ for i = 1 : 1 : cantidadDias
     salidasDia(1, activado) = 1;
     
     % Indice de columnas de la matriz de activacion:
-    % 1-Horas de activacion
-    % 2-Irradiancia
-    % 3-Temperatura ambiente
-    % 4-Velocidad del viento
-    % 5-Temperatura en la superficie del panel
-    % 6-Potencia beneficio
+    % 1-Irradiancia
+    % 2-Temperatura ambiente
+    % 3-Velocidad del viento
     
     % Transpone la matriz individual del dia
     entradasDia = entradasDia';
@@ -73,34 +70,92 @@ for i = 1 : 1 : cantidadDias
 
 end
 
-%exportarDataset(rutaGuardadoDatasets, datasetTodosDias);
-exportarDatasetMinimo(rutaGuardadoDatasets, datasetTodosDias);
+%% Preparacion para exportar
+% Editar las siguientes dos lineas la cantidad de dias deseados en el
+% conjunto de datos resultante:
+cantidadDiasEntrenamiento = 30;
+cantidadDiasPrueba = 15;
+
+cantidadDiasExportar = cantidadDiasEntrenamiento + cantidadDiasPrueba;
+
+diasExportar = randperm(cantidadDias, cantidadDiasExportar);
+
+diasEntrenamiento = diasExportar(1 : cantidadDiasEntrenamiento);
+diasPrueba = diasExportar(cantidadDiasEntrenamiento + 1 : end);
+
+datasetEntrenamiento = seleccionDias(diasEntrenamiento, datasetTodosDias);
+datasetPrueba = seleccionDias(diasPrueba, datasetTodosDias);
+
+% Llamada a las funciones de exportacion
+exportarDatasetCompleto(rutaGuardadoDatasets, datasetTodosDias);
+exportarDatasetEntrenamiento(rutaGuardadoDatasets, datasetEntrenamiento, diasEntrenamiento);
+exportarDatasetPrueba(rutaGuardadoDatasets, datasetPrueba, diasPrueba);
 
 fprintf('La Bibliotecaria ha terminado de analizar %d perfiles óptimos en %f segundos \n\n', cantidadDias, toc);
 
+%% Seleccion de dias de entrenamiento/prueba desde el conjunto de datos
+function dataset = seleccionDias(dias, datasetTodosDias)
 
-function exportarDataset(rutaGuardadoDatasets, datasetTodosDias)
+    limiteSuperior = @(n) 720 * n + 1;
+    limiteInferior = @(m) 720 * m;
 
-    fileID = fopen([rutaGuardadoDatasets, 'datasetsPerfiles.txt'], 'w');
-    formatoEncabezado = ['%s %12s %12s %12s %12s %12s %12s\r\n'];
-    formatoDatos = ['%.4f %12.4f %12.4f %12.4f %12.4f %12.4f %12.0f\r\n'];
+    cantidadDias = length(dias);
+    dataset = zeros(cantidadDias * 720, 4);
 
-
-    fprintf(fileID, formatoEncabezado, 'hora','irrad', 'Tamb', 'vient', 'Tpan', 'Pben', 'Irrig');
-    fprintf(fileID, formatoDatos, datasetTodosDias');
-    fclose(fileID);
+    for i = 1 : cantidadDias
+        dataset(limiteSuperior(i - 1) : limiteInferior(i), :) = datasetTodosDias(limiteSuperior(dias(i) - 1) : limiteInferior(dias(i)), :);
+    end
 
 end
 
-function exportarDatasetMinimo(rutaGuardadoDatasets, datasetTodosDias)
+%% Exportar conjuntos de datos
+function exportarDatasetCompleto(rutaGuardadoDatasets, datasetTodosDias)
+%% exportarDataset
+% Exporta todos los datos disponibles en un unico archivo
 
-    fileID = fopen([rutaGuardadoDatasets, 'datasetTPTEST.txt'], 'w');
-    formatoEncabezado = ['%s %1s %1s %1s %1s %1s %1s\r\n'];
-    formatoDatos = ['%.4f %1.4f %1.4f %1.4f %1.4f %1.4f %1.0f\r\n'];
-    
-    fprintf(fileID, formatoEncabezado, 'R_0','R_1', 'R_2', 'R_3', 'R_4', 'R_5', 'Class');
-    fprintf(fileID, formatoDatos, datasetTodosDias(21601:43200, :)');
+    fileID = fopen([rutaGuardadoDatasets, 'datasetsPerfiles.txt'], 'w');
+    formatoEncabezado = ['%s %12s %12s %12s \r\n'];
+    formatoDatos = ['%.4f %12.4f %12.4f %12.0f\r\n'];
+
+    fprintf(fileID, formatoEncabezado, 'irrad', 'Tamb', 'vient', 'Irrig');
+    fprintf(fileID, formatoDatos, datasetTodosDias');
     fclose(fileID);
+    
+    fprintf('La Bibliotecaria ha exportado todos los perfiles como un conjunto de datos en %s\n', rutaGuardadoDatasets);
+
+end
+
+function exportarDatasetEntrenamiento(rutaGuardadoDatasets, datasetDiasEntrenamiento, diasEntrenamiento)
+%% exportarDatasetEntrenamiento
+% Exporta solo los dias de entrenamiento indicados en un archivo
+
+    fileID = fopen([rutaGuardadoDatasets, 'datasetTP.txt'], 'w');
+    formatoEncabezado = ['%s %1s %1s %1s\r\n'];
+    formatoDatos = ['%.4f %1.4f %1.4f %1.0f\r\n'];
+    
+    fprintf(fileID, formatoEncabezado, 'R_0','R_1', 'R_2', 'Class');
+    fprintf(fileID, formatoDatos, datasetDiasEntrenamiento');
+    fclose(fileID);
+    
+    fprintf('\nLa Bibliotecaria ha exportado los perfiles de entrenamiento en un conjunto de datos en %s de los siguientes dias: \n', rutaGuardadoDatasets);
+    disp(diasEntrenamiento);
+    
+end
+
+function exportarDatasetPrueba(rutaGuardadoDatasets, datasetDiasPrueba, diasPrueba)
+%% exportarDatasetMinimo
+% Exporta solo los dias de prueba indicados en un archivo
+
+    fileID = fopen([rutaGuardadoDatasets, 'datasetTP_PRUEBA.txt'], 'w');
+    formatoEncabezado = ['%s %1s %1s %1s\r\n'];
+    formatoDatos = ['%.4f %1.4f %1.4f %1.0f\r\n'];
+    
+    fprintf(fileID, formatoEncabezado, 'R_0','R_1', 'R_2', 'Class');
+    fprintf(fileID, formatoDatos, datasetDiasPrueba');
+    fclose(fileID);
+    
+    fprintf('\nLa Bibliotecaria ha exportado los perfiles de prueba en un conjunto de datos en %s de los siguientes dias: \n', rutaGuardadoDatasets);
+    disp(diasPrueba);
 end
 
 
