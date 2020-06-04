aa2803003f858a75efa1a93fc2ff0046881d0127
James Perez <danjame@hotmail.com>
2020-06-04 09:00:50 -0500

    RBA - Relieff: ejecutarRelieff

--- SBR/RBA/relieff.py
+++ SBR/RBA/relieff.py
@@ -1,5 +1,48 @@
 """
 TANGENTE PENITENTE
-Nombre: 
-Descripcion: 
+- Nombre: relieff.py
+- Descripcion: El algoritmo RelieF calcula el puntaje de cada atributo
+evaluando su fortaleza basada en los k-vecinos mas cercano. Devuelve
+una lista de puntajes de atributos.
+---------------------------------------------------------------------------------------------------------------------------------------------------
+ReBATE v1.0: incluye un código Python autónomo para ejecutar cualquiera de los algoritmos basados en Relief incluidos/disponibles diseñados para 
+el filtrado/clasificación de atributos. Estos algoritmos son una forma rápida de identificar los atributos en el conjunto de datos que pueden ser 
+más importantes para predecir algún endpoint fenotípico. Estos scripts producen un conjunto ordenado de nombres de atributos, junto con sus 
+respectivas puntuaciones (determinadas de forma única por el algoritmo particular seleccionado). Ciertos algoritmos requieren que se especifiquen 
+los parámetros clave de ejecución. Este código se basa en gran medida en los algoritmos basados en Relief implementados en el software de reducción 
+de la dimensionalidad multifactorial (MDR). Sin embargo, estas implementaciones se han ampliado para dar cabida a los atributos continuos 
+(y a los atributos continuos mezclados con los atributos discretos) así como a un endpoint continuo. Este código también tiene en cuenta los puntos
+de datos que faltan. Construido en este código, hay una estrategia para detectar automáticamente a partir de los datos cargados, estas 
+características relevantes.
+---------------------------------------------------------------------------------------------------------------------------------------------------------
 """
+
+def EjecutarReliefF(parameter_list):
+    pass
+
+def ReliefF(parameter_list):
+    pass
+
+def calcularMatrizDistancias(parameter_list):
+    pass
+
+def hacerMapaMulticlases(parameter_list):
+    pass
+
+def encontrarVecinosMasCercanos_ReliefFMulticlase(parameter_list):
+    pass
+
+def encontrarVecinosMasCercanos_ReliefFContinuo(parameter_list):
+    pass
+
+def encontrarVecinosMasCercanos_ReliefFDiscreto(parameter_list):
+    pass
+
+def evaluarReliefF(parameter_list):
+    pass
+
+def encontrarIndiceMax(parameter_list):
+    pass
+
+def calcularDistancia(parameter_list):
+    pass
