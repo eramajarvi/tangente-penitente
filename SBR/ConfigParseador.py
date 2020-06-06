c09f550c4279d92502ead3ed98f46c0a4007613b
James Perez <danjame@hotmail.com>
2020-06-06 11:43:34 -0500

    ConfigParser: emieza implementacion

--- SBR/ConfigParseador.py
+++ SBR/ConfigParseador.py
@@ -6,3 +6,21 @@ Maneja el archivo de configuración cargando, analizando y
 pasando sus valores a Constantes. También incluye un método para 
 generar conjuntos de datos para la validación cruzada.
 """
+
+from Constantes import *
+import os
+import copy
+import random
+
+class ConfigParseador:
+    def __init__(self, nombreArchivo):
+        pass
+
+    def parsearConfiguracion(self, nombreArchivo):
+        pass
+
+    def PartVC(self):
+        pass
+
+    def hacerParticiones(self, listaVC, numParticiones, rutaArchivo, listaEncabezados):
+        pass
