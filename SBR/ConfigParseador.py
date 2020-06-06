611c344e7c24fb25b71389ab7825f9d3a6dd6283
James Perez <danjame@hotmail.com>
2020-06-06 11:47:32 -0500

    ConfigParser: implementado __init__

--- SBR/ConfigParseador.py
+++ SBR/ConfigParseador.py
@@ -14,7 +14,29 @@ import random
 
 class ConfigParseador:
     def __init__(self, nombreArchivo):
-        pass
+        
+        # Siempre se usa la misma semilla aqui, de tal forma que
+        # los mismos conjuntos de datos de VC seran generados si se
+        # ejecuta de nuevo
+        random.seed(1)
+
+        self.carComentario = '#'
+        self.carParametro = '='
+        
+        # Parsea el archivo de configuracion y obtiene todos los
+        # parametros
+        self.parametros = self.parsearConfiguracion(nombreArchivo)
+        
+        # Construye la clase Constantes usando los parametros del
+        # archivo de configuracion
+        cons.fijarConstantes(self.parametros)
+
+        if cons.validacionCruzadaInterna == 0 or cons.validacionCruzadaInterna == 1:
+            pass
+        
+        # Hacer VC interna
+        else:
+            self.PartVC()
 
     def parsearConfiguracion(self, nombreArchivo):
         pass
