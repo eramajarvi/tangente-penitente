314e4b8d5b455fe35e59943e138ee54038ab721b
James Perez <danjame@hotmail.com>
2020-06-06 21:16:39 -0500

    ConfigParseador: implementado PartVC

--- SBR/ConfigParseador.py
+++ SBR/ConfigParseador.py
@@ -77,7 +77,135 @@ class ConfigParseador:
         return parametros
 
     def PartVC(self):
-        pass
+        """ Dado un conjunto de datos, partCV lo particiona 
+        aleatoriamente en X particiones aleatorias balanceadas
+        para validacion cruzada que son guardadas en el archivo
+        especificado.
+        #PARAM rutaArchivo - especifica la ruta y el nombre de
+        los nuevos conjuntos de datos"""
+
+        numParticiones = cons.validacionCruzadaInterna
+        nombreCarpeta = copy.deepcopy(cons.archivoEntrenamiento)
+        nombreArchivo = nombreCarpeta.split('\\')
+        nombreArchivo = nombreArchivo[len(nombreArchivo) - 1]
+        rutaArchivo = nombreCarpeta + '\\' + nombreArchivo
+
+        # Hacer carpeta para conjuntos de datos para VC
+        if not os.path.exists(nombreCarpeta):
+            os.mkdir(nombreCarpeta)
+
+        # Abrir el archivo de datos especificado
+        try:
+            a = open(cons.archivoEntrenamiento + '.txt', 'rU')
+
+        except Exception as inst:
+            print(type(inst))
+            print(inst.args)
+            print(inst)
+            print('No se pudo abrir', cons.archivoEntrenamiento + '.txt')
+            raise
+
+        else:
+            listaConjuntoDatos = []
+            # Remover la primera fila (encabezados)
+            listaEncabezados = a.readline().rstrip('\n').split('\t')
+
+            for linea in a:
+                listaLineas = linea.strip('\n').split('\t')
+                listaConjuntoDatos.append(listaLineas)
+
+            a.close()
+
+        longitudDatos = len(listaConjuntoDatos)
+
+        # Caracterizar el fenotipo 
+        fenotipoDiscreto = True
+
+        if cons.etiquetaFenotipo in listaEncabezados:
+            refFenotipo = listaEncabezados.index(cons.etiquetaFenotipo)
+
+        else:
+            print('ConfigParseador - Error: No se encuentra la etiqueta del fenotipo.')
+
+            inst = 0
+            diccionarioClases = {}
+
+            # Revisa que discriminar entre atributos discretos y
+            # continuos
+            while len(list(diccionarioClases.keys())) <+ cons.limiteAtributoDiscreto and inst < longitudDatos:
+                objetivo = listaConjuntoDatos[inst][refFenotipo]
+
+                # Revisa si ya se ha visto antes este estado de
+                # atributo
+                if objetivo in list(diccionarioClases.keys()):
+                    diccionarioClases[object] += 1
+
+                else:
+                    diccionarioClases[objetivo] = 1
+
+                inst += 1
+
+            if (len(list(diccionarioClases.keys))) > cons.limiteAtributoDiscreto:
+                fenotipoDiscreto = False
+
+            else:
+                pass
+
+            # Guarda todas las particiones
+            listaVC = []
+
+            for x in range(numParticiones):
+                listaVC.append([])
+
+            if fenotipoDiscreto:
+                listaMaestra = []
+                llavesClase = list(diccionarioClases.keys())
+
+                for i in range(len(llavesClase)):
+                    listaMaestra.append([])
+
+                for i in listaConjuntoDatos:
+                    noEncontrado = True
+                    j = 0
+
+                    while noEncontrado:
+                        if i[refFenotipo] == llavesClase[j]:
+                            listaMaestra[j].append(i)
+                            noEncontrado = False
+                        
+                        j += 1
+
+                # Aleatoriza instancias de clase antes de particionar
+                from random import shuffle
+
+                for i in range(len(llavesClase)):
+                    shuffle(listaMaestra[i])
+
+                for claseActual in listaMaestra:
+                    partActual = 0
+                    contador = 0
+
+                    for x in claseActual:
+                        listaVC[partActual].append(x)
+                        contador += 1
+                        partActual = contador % numParticiones
+
+                self.hacerParticiones(listaVC, numParticiones, rutaArchivo, listaEncabezados)
+
+            # Atributo continuo
+            else:
+                from random import shuffle
+                shuffle(listaConjuntoDatos)
+
+                partActual = 0
+                contador = 0
+
+                for x in listaConjuntoDatos:
+                    listaVC[partActual].append(x)
+                    contador += 1
+                    partActual = contador % numParticiones
+
+                self.hacerParticiones(listaVC, numParticiones, rutaArchivo, listaEncabezados)
 
     def hacerParticiones(self, listaVC, numParticiones, rutaArchivo, listaEncabezados):
         pass
