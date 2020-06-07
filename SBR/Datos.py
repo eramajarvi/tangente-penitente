e91068b000e75b0d8c9bf62d8fc620cf57958b9e
James Perez <danjame@hotmail.com>
2020-06-06 23:10:29 -0500

    Datos: empieza implementacion del modulo

--- SBR/Datos.py
+++ SBR/Datos.py
@@ -2,7 +2,58 @@
 # Datos.py
 
 """
-Carga el conjunto de datos, caracteriza y almacena las características críticas de los conjuntos de datos (incluyendo los atributos discretos 
-frente a los continuos y el fenotipo), maneja los datos que faltan y, finalmente, da formato a los datos para que puedan ser convenientemente 
-utilizados por Tangente Penitente.
+Carga el conjunto de datos, caracteriza y almacena las 
+características críticas de los conjuntos de datos (incluyendo 
+los atributos discretos frente a los continuos y el fenotipo), 
+maneja los datos que faltan y, finalmente, da formato a los 
+datos para que puedan ser convenientemente utilizados por 
+Tangente Penitente.
 """
+
+import math
+import random
+import sys
+from Constantes import *
+
+class GestionDatos:
+    def __init__(self, archivoEntrenamiento, archivoPrueba, listaInfo = None):
+        pass
+
+    def cargarDatos(self, archivoDatos, hacerEntrenamiento):
+        pass
+
+    def caracterizarConjuntoDatos(self, datosEntrenamientoCrudos):
+        pass
+
+    def discriminarFenotipo(self, datosCrudos):
+        pass
+
+    def discriminarClases(self, datosCrudos):
+        pass
+
+    def compararConjuntoDatos(self, datosPruebaCrudos):
+        pass
+
+    def discriminarAtributos(self, datosCrudos):
+        pass
+
+    def caracterizarAtributos(self, datosCrudos):
+        pass
+
+    def calcularDE(self, listaFenotipos):
+        pass
+
+    def formatearDatos(self, datosCrudos, entrenamiento):
+        pass
+
+    def guardarDatosTurfTemp(self):
+        pass
+
+    def regresarDatosCompletos(self):
+        pass
+
+    def gestionDatosTurf(self, puntajesFiltro, porcentajeTurf):
+        pass
+
+    def hacerConjuntoDatosFiltrado(self, atributosEnDatos, nombreArchivo, puntajesFiltro):
+        pass
