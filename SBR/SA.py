3f252528fa188fbef49dcab9c514ac15206668d2
James Perez <danjame@hotmail.com>
2020-06-06 22:22:50 -0500

    SA: inicia implementacion

--- SBR/SA.py
+++ SBR/SA.py
@@ -2,7 +2,36 @@
 # SA.py
 
 """
-Maneja el almacenamiento, la actualización y la aplicación de la heurística de seguimiento de atributos y del feedback. Esta estrategia fue
- propuesta y publicada por Ryan Urbanowicz, Ambrose Granizo-Mackenzie y Jason Moore en "Instance-Linked Attribute Tracking and Feedback for 
- Michigan-Style Supervised Learning Classifier Systems". [2012].
+Maneja el almacenamiento, la actualización y la aplicación de la 
+heurística de seguimiento de atributos y del feedback. Esta 
+estrategia fue propuesta y publicada por Ryan Urbanowicz, 
+Ambrose Granizo-Mackenzie y Jason Moore en "Instance-Linked 
+Attribute Tracking and Feedback for Michigan-Style Supervised 
+Learning Classifier Systems". [2012].
 """
+
+from Constantes import *
+import copy
+import random
+
+class SeguimientoAtributos:
+    def __init__(self, hacerSeguimientoAtributos):
+        super().__init__()
+
+    def actualizarPorcentaje(self, iterExplora):
+        pass
+
+    def actualizarSeguimientoAtributos(self, pob):
+        pass
+
+    def obtenerProbSeguimiento(self):
+        pass
+
+    def generarProbSeguimiento(self):
+        pass
+
+    def sumaSeguimientoAtributosGlobal(self):
+        pass
+
+    def reiniciarSA(self):
+        pass
