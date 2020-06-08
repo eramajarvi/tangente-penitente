e944ae3fd96d58d3e0fe805144e295ffc5f7a15c
James Perez <danjame@hotmail.com>
2020-06-08 15:12:31 -0500

    Updated: el arca. Ahora importa datos directamente de 12 horas

--- helperElArca.m
+++ helperElArca.m
@@ -1,16 +1,22 @@
-%%% Importar datos desde el archivo de Excel al espacio de trabajo en
+%% EL ARCA
+%%% Importa datos desde el archivo de Excel al espacio de trabajo en
 %%% Matlab
 clear all; 
 clc;
+
+tic
 
 % Ruta es el nombre del archivo donde están los datos a importar
 ruta = 'Datos-10minutos.xlsx';
 
 % Cantidad total del número de días que disponemos en los datos
-cantidadDias = 516;
+cantidadDias = 588;
 
-% Cantidad total de datos que se tienen por día
-cantidadDatosPorDia = 143;
+% Cantidad total de datos que se tienen por día de 6AM a 6PM
+% en 12 horas, cada 10 minutos, se tienen 73
+cantidadDatosPorDia = 73;
+
+diasCreados = 0;
 
 % Se inicializan las matrices de interés
 TemperaturaAmbiente_Matriz = zeros(cantidadDias, cantidadDatosPorDia);
@@ -22,11 +28,21 @@ for i = 1:cantidadDias
     numeroDia = i;
     dia = ['Dia', num2str(i)];
     
+    Hora_temp = xlsread(ruta, dia, 'B:B');
+    Hora_temp = Hora_temp(find(Hora_temp == 0.25):find(Hora_temp == 0.75));
     fprintf(['Calculando ' dia '...' '\n'])
     
-    TemperaturaAmbiente_Matriz(i, :) = [xlsread(ruta, dia, 'C:C')]';
-    VelocidadViento_Matriz(i, :) = [xlsread(ruta, dia, 'D:D')]';
-    Irradiancia_Matriz(i, :) = [xlsread(ruta, dia, 'E:E')]';
+    if length(Hora_temp) == 73
+        % Lee del archivo de Excel los datos
+        TemperaturaAmbiente_Matriz(i, :) = xlsread(ruta, dia, 'C37:C109')';
+        VelocidadViento_Matriz(i, :) = xlsread(ruta, dia, 'D37:D109')';
+        Irradiancia_Matriz(i, :) = xlsread(ruta, dia, 'E37:E109')';
+   
+        diasCreados = diasCreados + 1;
+    end
+    
 end
 
 save('MatricesDelArca', 'Irradiancia_Matriz', 'TemperaturaAmbiente_Matriz', 'VelocidadViento_Matriz');
+fprintf(['El Arca construyo ' diasCreados 'dias en ' toc 'segundos.']);
+
