e9b035df0d7fd2f826f4a4127edee3e57acacb65
James Perez <danjame@hotmail.com>
2020-06-08 21:53:33 -0500

    Updated: el arca. Importa correctamente datos de 12 horas.

--- helperElArca.m
+++ helperElArca.m
@@ -25,24 +25,36 @@ Irradiancia_Matriz = zeros(cantidadDias, cantidadDatosPorDia);
 
 for i = 1:cantidadDias
 
-    numeroDia = i;
+    numeroDia = j;
     dia = ['Dia', num2str(i)];
     
     Hora_temp = xlsread(ruta, dia, 'B:B');
     Hora_temp = Hora_temp(find(Hora_temp == 0.25):find(Hora_temp == 0.75));
+    
     fprintf(['Calculando ' dia '...' '\n'])
     
     if length(Hora_temp) == 73
+        fprintf('Calculando dia %d...\n', j)
+        
         % Lee del archivo de Excel los datos
-        TemperaturaAmbiente_Matriz(i, :) = xlsread(ruta, dia, 'C37:C109')';
-        VelocidadViento_Matriz(i, :) = xlsread(ruta, dia, 'D37:D109')';
-        Irradiancia_Matriz(i, :) = xlsread(ruta, dia, 'E37:E109')';
+        TemperaturaAmbiente_Matriz(j, :) = xlsread(ruta, dia, 'C37:C109')';
+        VelocidadViento_Matriz(j, :) = xlsread(ruta, dia, 'D37:D109')';
+        Irradiancia_Matriz(j, :) = xlsread(ruta, dia, 'E37:E109')';
    
         diasCreados = diasCreados + 1;
     end
     
+    j = j + 1;
+    
 end
 
+%% Limpiar la matriz de las filas vacias
+TemperaturaAmbiente_Matriz( ~any(TemperaturaAmbiente_Matriz, 2), : ) = [];
+VelocidadViento_Matriz( ~any(VelocidadViento_Matriz, 2), : ) = [];
+Irradiancia_Matriz( ~any(Irradiancia_Matriz, 2), : ) = [];
+
+%% Guardar datos
+
 save('MatricesDelArca', 'Irradiancia_Matriz', 'TemperaturaAmbiente_Matriz', 'VelocidadViento_Matriz');
-fprintf(['El Arca construyo ' diasCreados 'dias en ' toc 'segundos.']);
+fprintf('El Arca construyo %d dias de los %d disponibles en %f segundos.\n', diasCreados, toc, cantidadDias);
 
