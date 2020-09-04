e73a0f2b38d6f4d13a5aeeffdbe5e81b09a547e3
James Perez <danjame@hotmail.com>
2020-09-04 15:41:35 -0500

    CE: inicia implementación

--- SBR/CE.py
+++ SBR/CE.py
@@ -7,3 +7,25 @@ en probabilidades para orientar el mecanismo de covering en la determinación de
 estrategia fue propuesta y publicada por Ryan Urbanowicz, Delaney Granizo-Mackenzie y Jason Moore en "Using Expert Knowledge to Guide Covering and 
 Mutation in a Michigan Style Learning Classifier System to Detecting Epistasis and Heterogeneity". [2012].
 """
+
+import os
+import copy
+import time
+from RBA.multisurf import *
+from RBA.relieff import *
+from RBA.surf import *
+from RBA.surfstar import *
+from RBA.turf_envoltorio import *
+
+class ConocimientoExperto:
+    def __init__(self, cons):
+        pass
+
+    def ajustarPuntajes(self, cons):
+        pass
+
+    def ejecutarFiltros(self, cons):
+        pass
+
+    def cargarCE(self, nombreRuta, cons):
+        pass
