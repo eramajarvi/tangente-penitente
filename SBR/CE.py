c209bc3c6a400045f319ece78a5996c917155a96
James Perez <danjame@hotmail.com>
2021-01-11 20:27:59 -0500

    CE: implementado ejecutarFiltros

--- SBR/CE.py
+++ SBR/CE.py
@@ -8,6 +8,10 @@ estrategia fue propuesta y publicada por Ryan Urbanowicz, Delaney Granizo-Macken
 Mutation in a Michigan Style Learning Classifier System to Detecting Epistasis and Heterogeneity". [2012].
 """
 
+from SBR.RBA.relieff import EjecutarReliefF
+from SBR.RBA.surfstar import EjecutarSURFStar
+from SBR.RBA.multisurf import Ejecutar_MultiSURF
+from SBR.RBA.turf_envoltorio import TurfEnvoltorio
 import os
 import copy
 import time
@@ -81,7 +85,100 @@ class ConocimientoExperto:
                 self.puntajes[i] = self.puntajes[i] - CEmin + cons.aptitudInicial
 
     def ejecutarFiltros(self, cons):
-        pass
+        """ Ejecuta el algoritmo filtro especificado y crea a la salida un archivo de texto filtro en el mismo formato que el software MDR (Reduccion de dimensionalidad multifactor / multifactor dimmensionality reduction) """
+        nombreArchivo = str(cons.nombrearchivoSalidaCE) + '_' + str(cons.algoritmoFiltro) + '_puntajes.txt'
+
+        if not os.path.exists(nombreArchivo):
+            referenciaInicioGlobal = time.time()
+            nombreEtiqueta = None
+            foo = None
+            bar = None
+
+            if cons.algoritmoFiltro == "multisurf_turf":
+                turf = TurfEnvoltorio(cons.amb, cons.algoritmoFiltro, cons.porcentajeTurf, foo, bar)
+                puntajesFiltro = turf.puntajesFiltro
+                nombreEtiqueta = 'MultiSURF_TuRF'
+
+            elif cons.algoritmoFiltro == "surfstar_turf":
+                turf = TurfEnvoltorio(cons.amb, cons.algoritmoFiltro, cons.porcentajeTurf, cons.fraccionMuestreoRelief, foo)
+                puntajesFiltro = turf.puntajesFiltro
+                nombreEtiqueta = 'SURFStar_TuRF'
+
+            elif cons.algoritmoFiltro == "surf_turf":
+                turf = TurfEnvoltorio(cons.amb, cons.algoritmoFiltro, cons.porcentajeTurf, cons.fraccionMuestreoRelief, foo)
+                puntajesFiltro = turf.puntajesFiltro
+                nombreEtiqueta = 'SURF_TuRF'
+
+            elif cons.algoritmoFiltro == "relieff_turf":
+                turf = TurfEnvoltorio(cons.amb, cons.algoritmoFiltro, cons.porcentajeTurf, cons.fraccionMuestreoRelief, cons.vecinosRelief)
+                puntajesFiltro = turf.puntajesFiltro
+                nombreEtiqueta = 'ReliefF_TuRF'
+
+            elif cons.algoritmoFiltro == "multisurf":
+                puntajesFiltro = Ejecutar_MultiSURF(cons.amb.datosFormateados)
+                nombreEtiqueta = 'MultiSURF'
+
+            elif cons.algoritmoFiltro == "surfstar":
+                puntajesFiltro = EjecutarSURFStar(cons.amb.datosFormateados, cons.fraccionMuestreoRelief)
+                nombreEtiqueta = 'SURFStar'
+
+            elif cons.algoritmoFiltro == "relieff":
+                algoritmoFiltro = EjecutarReliefF(cons.amb.datosFormateados, cons.fraccionMuestreoRelief, cons.vecinosRelief)
+                nombreEtiqueta = 'ReliefF'
+
+            else:
+                print("Conocimiento Experto - ERROR: Algoritmo no encontrado.")
+
+            tiempoGlobal = (time.time() - referenciaInicioGlobal)
+
+            # Salida del archivo de los puntajes de atributos basados en ReliefF
+
+            try:
+                filtroSalida = open(nombreArchivo, 'w')
+
+            except Exception as inst:
+                print(type(inst))
+                print(inst.args)
+                print(inst)
+                print('No se puede abrir', nombreArchivo)
+                raise
+
+            print(puntajesFiltro)
+
+            filtroSalida.write(str(nombreEtiqueta) + " Analisis completado con Tangente Penitente" + '\n')
+            filtroSalida.write("Tiempo de ejecucion [s]:" + str(tiempoGlobal) + '\n')
+            filtroSalida.write("=== PUNTAJES ===" + '\n')
+            listaPuntajes = []
+
+            for i in range(len(puntajesFiltro)):
+                listaPuntajes.append([cons.amb.datosFormateados.listaEncabezadoEntrenamiento[i], puntajesFiltro[i]])
+
+            listraFiltrosOrdenada = sorted(listaPuntajes, key=lambda test:test[1], reverse=True)
+
+            for i in range(len(listraFiltrosOrdenada)):
+                filtroSalida.write(str(listraFiltrosOrdenada[i][0]) + '\t')
+                filtroSalida.write(str(listraFiltrosOrdenada[i][1]) + '\t')
+                filtroSalida.write(str(i + 1) + '\n')
+
+            filtroSalida.close()
+            return puntajesFiltro
+
+            # -------------------------------------------------
+
+        # El archivo de puntajes de atributos ya existe - entonces se carga el archivo existente y se procede a volver a ejecutar el algoritmo de puntajes de atributos
+        else:
+            try:
+                listaValoresOrdenados = self.cargarCE(cons.nombreArchivoSalidaCE + '_' + str(cons.algoritmoFiltro) + '_puntajes.txt', cons)
+
+            except Exception as inst:
+                print(type(inst))
+                print(inst.args)
+                print(inst)
+                print('No se pudo abrir', cons.nombreArchivoSalidaCE + '_' + str(cons.algoritmoFiltro) + '_puntajes.txt')
+
+            else:
+                return listaValoresOrdenados
+
 
     def cargarCE(self, nombreRuta, cons):
         pass
