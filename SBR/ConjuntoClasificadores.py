576cbc7854b03377b2ed890558a1b7358c1480db
James Perez <danjame@hotmail.com>
2021-01-11 21:46:02 -0500

    ConjuntoClasificadores: definidos metodos de la clase

--- SBR/ConjuntoClasificadores.py
+++ SBR/ConjuntoClasificadores.py
@@ -7,4 +7,101 @@ y la heurística que actúan sobre estos conjuntos.
 """
 
 from SBR.constantes import *
+from SBR.clasificador import Clasificador
+
+import random
+import copy
+import sys
+
+class ConjuntoClasificadores:
+    def __init__(self, a = None):
+        
+        pass
+
+    def crearPoblacion(self):
+        pass
+
+    def reiniciarPoblacion(self, archivoReinicio):
+        pass
+
+    def hacerConjuntoCoincidencias(self, estadoFenotipo, iterExplor):
+        pass
+
+    def hacerConjuntoCorrectos(self, fenotipo):
+        pass
+
+    def hacerEvalConjuntoCoincidencias(self, estado):
+        pass
+
+    def eliminacion(self, iterExplor):
+        pass
+
+    def eliminarDePoblacion(self):
+        pass
+
+    def eliminarMacroClasificador(self, ref):
+        pass
+
+    def eliminarDelConjuntoCoincidencias(self, refEliminar):
+        pass
+
+    def eliminarDelConjuntoCorrectos(self, refEliminar):
+        pass
+
+    def ejecutarAG(self, iterExplor, estado, fenotipo):
+        pass
+
+    def seleccionarClasificadorRuleta(self):
+        pass
+
+    def seleccionarClasificadorT(self):
+        pass
+
+    def subsumirClasificador(self, cl = None, cl1P = None, cl2P = None):
+        pass
+
+    def subsumirClasificador2(self, cl):
+        pass
+
+    def hacerSubsuncionCorrecta(self):
+        pass
+
+    def agregarClasificadorAPoblacion(self, cl, covering):
+        pass
+
+    def insertarClasificadoresDescubiertos(self, cl1, cl2, clP1, clP2, iterExplor):
+        pass
+
+    def actualizarConjuntos(self, iterExplor):
+        pass
+
+    def ObtenerPromedioEstampaIter(self):
+        pass
+
+    def fijarEstampasIter(self, iterExplor):
+        pass
+
+    def obtenerSumaAptitud(self, listaConjuntos):
+        pass
+
+    def obtenerSumaAptitudPoblacion(self):
+        pass
+
+    def obtenerClasificadoresIdenticos(self, nuevoCl):
+        pass
+
+    def limpiarConjuntos(self):
+        pass
+
+    def ejecutarEvaluacionPoblacionPromedio(self, iterExplor):
+        pass
+
+    def ejecutarSumaGeneralidadAtrib(self):
+        pass
+
+    def recalcularSumaNumerosidad(self):
+        pass
+
+    def obtenerSeguimientoPob(self, precision, iterExplor, frecuenciaSeguimiento):
+        pass
 
